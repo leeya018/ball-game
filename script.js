58ac69642c9db272ee6afe630a00e1db9cae6292
leeya018 <leeyahav018@gmail.com>
2021-02-14 18:39:38 +0200

    put all files in one

--- script.js
+++ script.js
@@ -1,5 +1,206 @@
-import { Timer } from "./timer.js";
-import { MovingElement, Element } from "./element.js";
+// import { Timer } from "./timer.js";
+// import { MovingElement, Element } from "./element.js";
+
+export class Timer {
+  constructor(className, time, el) {
+    this.time = time;
+    this.className = className;
+    this.interval = null;
+    this.el = el;
+  }
+  
+  start(div) {
+    div.innerText = this.formatTimerTxt();
+    this.interval = setInterval(() => {
+      this.time += 1;
+      div.innerText = this.formatTimerTxt();
+  
+    }, 1000);
+  }
+  getHtml() {
+    let timerDiv = document.createElement("div");
+    timerDiv.classList.add(this.className, "timer-el");
+    return timerDiv;
+  }
+  reset() {
+    this.time = 0;
+  }
+
+  formatTimerTxt() {
+    let minutes = Math.floor(this.time / 60);
+    let seconds = this.time % 60;
+    let minutesStr = minutes < 10 ? "0" + minutes : minutes;
+    let secondsStr = seconds < 10 ? "0" + seconds : seconds;
+    return `${minutesStr}:${secondsStr}`;
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  removeHtml() {
+    this.el.remove();
+  }
+}
+
+
+
+
+const ELEMENT_SIZE = 100;
+const FREEZE_TIME = 4000;
+let id = 0;
+
+export class Element {
+  constructor(className) {
+    this.className = className;
+    this.element = null;
+    this.id = ++id;
+  }
+
+  getTopNumber(element) {
+    let el = element || this.element;
+    return parseInt(el.style.top.replace("px", ""));
+  }
+  getLeftNumber(element) {
+    let el = element || this.element;
+    return parseInt(el.style.left.replace("px", ""));
+  }
+
+  positionElementOnScreen() {
+    this.element = document.createElement("div");
+    this.element.classList.add("basket");
+
+    let heightLim = document.body.clientHeight;
+    let widthLim = document.body.clientWidth;
+
+    let top = this.chooseRandomLocation(heightLim) + "px";
+    let left = this.chooseRandomLocation(widthLim) + "px";
+
+    this.element.style.top = top;
+    this.element.style.left = left;
+    this.element.style.display = "block";
+
+    document.body.append(this.element);
+  }
+  chooseRandomLocation(sizeLimit) {
+    let position = Math.floor(Math.random() * sizeLimit) - ELEMENT_SIZE;
+    return position < 0 ? position + ELEMENT_SIZE : position;
+  }
+}
+export class MovingElement extends Element {
+  constructor(className, points) {
+    super(className);
+    this.move = false;
+    this.points = points;
+    this.frozen = false;
+    this.timer = new Timer("timer", 0);
+  }
+
+  set move1(isMove) {
+    this.move = isMove;
+  }
+
+  checkCollision(myTop, myLeft) {
+    let basketElement = document.querySelector(".basket");
+    let top = this.getTopNumber(basketElement);
+    let bottom = top + ELEMENT_SIZE;
+    let left = this.getLeftNumber(basketElement);
+    let right = left + ELEMENT_SIZE;
+    // console.log(top, bottom, left, right);
+
+    let myRight = myLeft + ELEMENT_SIZE;
+    let myBottom = myTop + ELEMENT_SIZE;
+    // console.log(myTop, myBottom, myLeft, myRight);
+
+    if (myTop > top && myTop < bottom && myLeft < right && myLeft > left) {
+      return true;
+    }
+    if (myTop > top && myTop < bottom && myRight < right && myRight > left) {
+      return true;
+    }
+    if (
+      myBottom > top &&
+      myBottom < bottom &&
+      myRight < right &&
+      myRight > left
+    ) {
+      return true;
+    }
+    if (
+      myBottom > top &&
+      myBottom < bottom &&
+      myLeft < right &&
+      myLeft > left
+    ) {
+      return true;
+    }
+  }
+
+  // positionElementOnScreen() {
+  //   this.element = document.createElement("div");
+  //   this.element.classList.add("basket");
+
+  //   let heightLim = document.body.clientHeight;
+  //   let widthLim = document.body.clientWidth;
+
+  //   let top = this.chooseRandomLocation(heightLim) + "px";
+  //   let left = this.chooseRandomLocation(widthLim) + "px";
+
+  //   this.element.style.top = top;
+  //   this.element.style.left = left;
+  //   this.element.style.display = "block";
+
+  //   document.body.append(this.element);
+
+  // }
+  positionElementOnScreen() {
+    this.element = document.createElement("div");
+    this.element.classList.add("ball", this.className);
+    let heightLim = document.body.clientHeight;
+    let widthLim = document.body.clientWidth;
+    let top, left;
+
+    do {
+      top = this.chooseRandomLocation(heightLim);
+      left = this.chooseRandomLocation(widthLim);
+    } while (this.checkCollision(top, left));
+
+    this.element.style.top = top + "px";
+    this.element.style.left = left + "px";
+    this.element.style.display = "block";
+
+    document.body.append(this.element);
+  }
+
+  freeze() {
+    this.frozen = true;
+    let xDiv = document.createElement("div");
+    xDiv.classList.add("frozen");
+    this.element.append(xDiv);
+
+    setTimeout(() => {
+      xDiv.classList.toggle("frozen");
+      xDiv.remove();
+      this.frozen = false;
+      let timerDiv = this.timer.getHtml();
+      this.element.append(timerDiv);
+      this.timer.start(timerDiv);
+    }, FREEZE_TIME);
+  }
+
+  updateElementLocation(e) {
+    if (this.move) {
+      this.element.style.top = e.clientY - ELEMENT_SIZE / 2 + "px";
+      this.element.style.left = e.clientX - ELEMENT_SIZE / 2 + "px";
+
+      let top = this.getTopNumber();
+      let left = this.getLeftNumber();
+      return { top, left };
+    }
+    return null;
+  }
+}
+
 
 let scoreDiv = document.querySelector(".score");
 let timerDiv = document.querySelector(".timer");
